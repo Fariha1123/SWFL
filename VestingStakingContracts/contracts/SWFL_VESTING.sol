b31c78ad0e8d99adc942d419fd6e6e495c173e21
Fariha Abbasi <farihaabbasi1123@gmail.com>
2020-08-29 16:01:47 +0500

    Dapp and contracts added initial commit

---
+++ VestingStakingContracts/contracts/SWFL_VESTING.sol
@@ -0,0 +1,275 @@
+pragma solidity ^0.6.0;
+// SPDX-License-Identifier: UNLICENSED
+
+/**
+ * @dev Wrappers over Solidity's arithmetic operations with added overflow
+ * checks.
+ *
+ * Arithmetic operations in Solidity wrap on overflow. This can easily result
+ * in bugs, because programmers usually assume that an overflow raises an
+ * error, which is the standard behavior in high level programming languages.
+ * `SafeMath` restores this intuition by reverting the transaction when an
+ * operation overflows.
+ *
+ * Using this library instead of the unchecked operations eliminates an entire
+ * class of bugs, so it's recommended to use it always.
+ */
+library SafeMath {
+    /**
+     * @dev Returns the addition of two unsigned integers, reverting on
+     * overflow.
+     *
+     * Counterpart to Solidity's `+` operator.
+     *
+     * Requirements:
+     * - Addition cannot overflow.
+     */
+    function add(uint256 a, uint256 b) internal pure returns (uint256) {
+        uint256 c = a + b;
+        require(c >= a, "SafeMath: addition overflow");
+
+        return c;
+    }
+
+    /**
+     * @dev Returns the subtraction of two unsigned integers, reverting on
+     * overflow (when the result is negative).
+     *
+     * Counterpart to Solidity's `-` operator.
+     *
+     * Requirements:
+     * - Subtraction cannot overflow.
+     */
+    function sub(uint256 a, uint256 b) internal pure returns (uint256) {
+        return sub(a, b, "SafeMath: subtraction overflow");
+    }
+
+    /**
+     * @dev Returns the subtraction of two unsigned integers, reverting with custom message on
+     * overflow (when the result is negative).
+     *
+     * Counterpart to Solidity's `-` operator.
+     *
+     * Requirements:
+     * - Subtraction cannot overflow.
+     *
+     * _Available since v2.4.0._
+     */
+    function sub(uint256 a, uint256 b, string memory errorMessage) internal pure returns (uint256) {
+        require(b <= a, errorMessage);
+        uint256 c = a - b;
+
+        return c;
+    }
+
+    /**
+     * @dev Returns the multiplication of two unsigned integers, reverting on
+     * overflow.
+     *
+     * Counterpart to Solidity's `*` operator.
+     *
+     * Requirements:
+     * - Multiplication cannot overflow.
+     */
+    function mul(uint256 a, uint256 b) internal pure returns (uint256) {
+        // Gas optimization: this is cheaper than requiring 'a' not being zero, but the
+        // benefit is lost if 'b' is also tested.
+        // See: https://github.com/OpenZeppelin/openzeppelin-contracts/pull/522
+        if (a == 0) {
+            return 0;
+        }
+
+        uint256 c = a * b;
+        require(c / a == b, "SafeMath: multiplication overflow");
+
+        return c;
+    }
+
+    /**
+     * @dev Returns the integer division of two unsigned integers. Reverts on
+     * division by zero. The result is rounded towards zero.
+     *
+     * Counterpart to Solidity's `/` operator. Note: this function uses a
+     * `revert` opcode (which leaves remaining gas untouched) while Solidity
+     * uses an invalid opcode to revert (consuming all remaining gas).
+     *
+     * Requirements:
+     * - The divisor cannot be zero.
+     */
+    function div(uint256 a, uint256 b) internal pure returns (uint256) {
+        return div(a, b, "SafeMath: division by zero");
+    }
+
+    /**
+     * @dev Returns the integer division of two unsigned integers. Reverts with custom message on
+     * division by zero. The result is rounded towards zero.
+     *
+     * Counterpart to Solidity's `/` operator. Note: this function uses a
+     * `revert` opcode (which leaves remaining gas untouched) while Solidity
+     * uses an invalid opcode to revert (consuming all remaining gas).
+     *
+     * Requirements:
+     * - The divisor cannot be zero.
+     *
+     * _Available since v2.4.0._
+     */
+    function div(uint256 a, uint256 b, string memory errorMessage) internal pure returns (uint256) {
+        // Solidity only automatically asserts when dividing by 0
+        require(b > 0, errorMessage);
+        uint256 c = a / b;
+        // assert(a == b * c + a % b); // There is no case in which this doesn't hold
+
+        return c;
+    }
+
+    /**
+     * @dev Returns the remainder of dividing two unsigned integers. (unsigned integer modulo),
+     * Reverts when dividing by zero.
+     *
+     * Counterpart to Solidity's `%` operator. This function uses a `revert`
+     * opcode (which leaves remaining gas untouched) while Solidity uses an
+     * invalid opcode to revert (consuming all remaining gas).
+     *
+     * Requirements:
+     * - The divisor cannot be zero.
+     */
+    function mod(uint256 a, uint256 b) internal pure returns (uint256) {
+        return mod(a, b, "SafeMath: modulo by zero");
+    }
+
+    /**
+     * @dev Returns the remainder of dividing two unsigned integers. (unsigned integer modulo),
+     * Reverts with custom message when dividing by zero.
+     *
+     * Counterpart to Solidity's `%` operator. This function uses a `revert`
+     * opcode (which leaves remaining gas untouched) while Solidity uses an
+     * invalid opcode to revert (consuming all remaining gas).
+     *
+     * Requirements:
+     * - The divisor cannot be zero.
+     *
+     * _Available since v2.4.0._
+     */
+    function mod(uint256 a, uint256 b, string memory errorMessage) internal pure returns (uint256) {
+        require(b != 0, errorMessage);
+        return a % b;
+    }
+    
+    function ceil(uint a, uint m) internal pure returns (uint r) {
+    return (a + m - 1) / m * m;
+    }
+}
+
+// ----------------------------------------------------------------------------
+// Owned contract
+// ----------------------------------------------------------------------------
+contract Owned {
+    address payable public owner;
+
+    event OwnershipTransferred(address indexed _from, address indexed _to);
+
+    constructor() public {
+        owner = msg.sender;
+    }
+
+    modifier onlyOwner {
+        require(msg.sender == owner);
+        _;
+    }
+
+    function transferOwnership(address payable _newOwner) public onlyOwner {
+        owner = _newOwner;
+        emit OwnershipTransferred(msg.sender, _newOwner);
+    }
+}
+
+// ----------------------------------------------------------------------------
+// ERC Token Standard #20 Interface
+// ----------------------------------------------------------------------------
+interface IERC20 {
+    function totalSupply() external view returns (uint256);
+    function balanceOf(address tokenOwner) external view returns (uint256 balance);
+    function allowance(address tokenOwner, address spender) external view returns (uint256 remaining);
+    function transfer(address to, uint256 tokens) external returns (bool success);
+    function approve(address spender, uint256 tokens) external returns (bool success);
+    function transferFrom(address from, address to, uint256 tokens) external returns (bool success);
+
+    event Transfer(address indexed from, address indexed to, uint256 tokens);
+    event Approval(address indexed tokenOwner, address indexed spender, uint256 tokens);
+}
+
+contract Vesting is Owned{
+    using SafeMath for uint256;
+    
+    address SWFL;
+    uint256 public lockingPeriod = 365 days;
+    uint256 public lockedTokens;
+    uint256 public lockedDate;
+    uint256 public claimedTokens;
+    
+    uint256 private cliff = 15 days;
+    
+    mapping(address => bool) whitelist;
+    
+    event AccountWhitelisted(address account);
+    event VestingStarted(uint256 tokens);
+    event TokensClaimed(uint256 tokens, address by);
+    
+    modifier isAllowed(address _account){
+        require(whitelist[_account] == true || _account == owner, "FAIL: Unauthorized");
+        _;
+    }
+    
+    constructor(address _SWFL) public{
+        SWFL = _SWFL;
+    }
+    
+    function LOCK(uint256 _tokens) public onlyOwner{
+        // require no more locking
+        require(IERC20(SWFL).balanceOf(address(this)) == 0, "FAIL: Already running lock");
+        // transfer tokens to contract from owner
+        IERC20(SWFL).transferFrom(msg.sender, address(this), _tokens);
+        
+        // update stats
+        lockedTokens = _tokens;
+        lockedDate = block.timestamp;
+        
+        emit VestingStarted(_tokens);
+    }
+    
+    function addToWhitelist(address _account) public onlyOwner{
+        whitelist[_account] = true;
+        emit AccountWhitelisted(_account);
+    }
+    
+    function CLAIM() public isAllowed(msg.sender){
+        // check, if any token is ready to be released
+        require(pendingRelease() > 0, "FAIL: Nothing pending to release");
+        // send tokens from contract to caller
+        IERC20(SWFL).transfer(msg.sender, pendingRelease());
+        
+        emit TokensClaimed(pendingRelease(), msg.sender);
+        
+        // update claim date
+        claimedTokens += pendingRelease();
+    }
+    
+    function pendingRelease() public view returns(uint256 _pendingRelease){
+        if(block.timestamp > lockedDate.add(cliff)){
+            uint256 totalTime = block.timestamp.sub(lockedDate.add(cliff));
+            totalTime = totalTime.div(1 days); // time in days
+            
+            if(totalTime > lockingPeriod)
+                totalTime = lockingPeriod;
+            
+            uint256 perDay = lockedTokens.div(lockingPeriod);
+            uint256 release = (totalTime.mul(perDay)).sub(claimedTokens);
+            
+            if(release == 0 && block.timestamp > lockedDate.add(lockingPeriod))
+                release = IERC20(SWFL).balanceOf(address(this));
+            
+            return release;
+        }
+        else return 0;
+    }
+}
